1197d2c790e69459abe96675cdacd8474e710127
ckstnr99 <ckstnr99@naver.com>
2024-09-11 09:51:49 +0900

    2024.09.11. 09:51

--- src/main/java/com/korit/senicare/repository/NurseRepository.java
+++ src/main/java/com/korit/senicare/repository/NurseRepository.java
@@ -7,5 +7,8 @@ import com.korit.senicare.entity.NurseEntity;
 
 @Repository
 public interface NurseRepository extends JpaRepository<NurseEntity, String> {
+
+    // 쿼리 작성 후 사용
+    boolean existsByUserId(String userId);
     
 }
