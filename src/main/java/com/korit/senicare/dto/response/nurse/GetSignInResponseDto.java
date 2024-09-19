6dda812ebefecfa3e1cde3d2709ee74a3e0d35e4
ckstnr99 <ckstnr99@naver.com>
2024-09-19 17:19:30 +0900

    2024.09.19. 17:19

--- src/main/java/com/korit/senicare/dto/response/nurse/GetSignInResponseDto.java
+++ src/main/java/com/korit/senicare/dto/response/nurse/GetSignInResponseDto.java
@@ -29,7 +29,7 @@ public class GetSignInResponseDto extends ResponseDto {
     // 리포지토리 -> 엔터티에서 값을 분해해서 가져올 필요없이 엔터티 자체를 가져와서 값을 넣어줌
 
 
-    // 클라이언트는 요청 헤더에 Bearer 인증 토큰을 포함하여 요청하고 회원가입이 성공적으로 이루어지면 
+    // 클라이언트는 요청 헤더에 Bearer 인증 토큰을 포함하여 요청하고 성공적으로 이루어지면 
     // 성공에 대한 응답으로 토큰에 해당하는 요양사의 아이디와 이름, 전화번호를 응답 받습니다.
     public static ResponseEntity<GetSignInResponseDto> success(NurseEntity nurseEntity) {
 
